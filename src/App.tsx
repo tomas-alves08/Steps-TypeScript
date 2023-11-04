e7302f39e402ff8d48b93d91eda2ec7d917f9a5f
Tomas Alves de Souza <tomas.alvesdesouza@datacom.co.nz>
2023-11-04 20:06:14 +1300

    Create Step App to Practice useState with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from "react";
 
-function App() {
+const messages: string[] = [
+  "Learn React",
+  "Apply for jobs",
+  "Invest your new income",
+];
+
+const App: FC = () => {
+  const [step, setStep] = useState<number>(1);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  function handleNext(e: React.FormEvent) {
+    e.preventDefault();
+    if (step < 3) setStep((prevStep) => prevStep + 1);
+  }
+
+  function handlePrevious(e: React.FormEvent) {
+    e.preventDefault();
+    if (step > 1) setStep((prevStep) => prevStep - 1);
+  }
+
+  function handleClose() {
+    setIsOpen((prev) => !prev);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
